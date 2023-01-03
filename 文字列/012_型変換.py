fbace4c7b6e68af560afd499ec60b3c43fcf40dd
Yuji Mitsumori <yuji.the.light.grows.thick@gmail.com>
2023-01-03 16:11:51 +0900

    関数ヘッダーのコメント変更

--- 文字列/012_型変換.py
+++ 文字列/012_型変換.py
@@ -1,12 +1,13 @@
-# 型変換
-#
-# 以下のデータ型を文字列型に変換しましょう。
-#
-# int:x = 1
-# float: x = 1.52
-# list: x = [1, 2, 0]
-# dict: x = dict(name = 'john', birth = 'US')
-#
+'''
+型変換
+
+以下のデータ型を文字列型に変換しましょう。
+
+int:x = 1
+float: x = 1.52
+list: x = [1, 2, 0]
+dict: x = dict(name = 'john', birth = 'US')
+'''
 
 x = 1
 print('整数型の文字列変換       = ' + str(x))
--- 文字列/013_包含関係.py
+++ 文字列/013_包含関係.py
@@ -1,10 +1,11 @@
-# 包含関係
-#
-# 以下の文字列Aが文字列Bに含まれているか、確認しましよう。
-#
-# A: 'sba'  B: 'gasba'
-# A: 'xdh'  B: 'orweit'
-#
+'''
+包含関係
+
+以下の文字列Aが文字列Bに含まれているか、確認しましよう。
+
+A: 'sba'  B: 'gasba'
+A: 'xdh'  B: 'orweit'
+'''
 
 a, b = 'sba' ,'gasba'
 print(a in b)
