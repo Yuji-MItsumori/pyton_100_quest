2ece08d5730e17f1ef990c5f058c162bcbe38681
Yuji Mitsumori <yuji.the.light.grows.thick@gmail.com>
2023-01-04 14:29:42 +0900

    コメントの追加

--- 02 リスト/019_ソート（昇順_降順）.py
+++ 02 リスト/019_ソート（昇順_降順）.py
@@ -6,9 +6,9 @@ numbers = [5, 0, 3, 5, 8, -4, 9, 1, -3, -4, 2]
 print(numbers)
 
 # 並び替えをした情報で新しくリストを作成する
-up_list = sorted(numbers)
+up_list = sorted(numbers)                   # 昇順
 print(up_list)
-dw_list = sorted(numbers, reverse=True)
+dw_list = sorted(numbers, reverse=True)     # 降順
 print(dw_list)
 
 # numbers自体を並び変える
